47b2294802d7cb3c34488bf0df168b7de1955a41
jolar <jolar@JOLA>
2021-10-10 15:55:11 -0400

    Add project files.

---
+++ FromAToB/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+
+namespace FromAToB
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int a, b, count;
+
+            string[] data = Console.ReadLine().Split();
+
+
+
+            a = int.Parse(data[0]);
+
+            b = int.Parse(data[1]);
+
+
+
+            if (a < b)
+
+                count = b - a;
+
+            else
+
+            {
+
+                count = 0;
+
+                while (a > b)
+
+                {
+
+                    if (a % 2 != 0)
+
+                        a += 1;
+
+                    else
+
+                        a /= 2;
+
+                    count++;
+
+                }
+
+
+
+                if (a < b)
+
+                {
+
+                    count += b - a;
+
+                }
+
+            }
+
+
+
+            Console.WriteLine(count);
+
+        }
+
+    }
+
+}
